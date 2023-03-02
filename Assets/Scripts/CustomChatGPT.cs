5f37b579857d4a32e04f9ddbec3b553ae7d67fb0
pvz <pvanzyl065@gmail.com>
2023-03-02 13:26:21 +0200

    Extend ChatGPTWrapper

---
+++ Assets/Scripts/CustomChatGPT.cs
@@ -0,0 +1,18 @@
+﻿using ChatGPTWrapper;
+
+namespace DD.API
+{
+	public class CustomChatGPT : ChatGPTConversation
+	{
+		// API endpoint and access token
+		public bool Ready => _selectedModel != null; 
+		public void SetupGPT(string apiKey, string chatBotName, string initialPrompt, int maxTokens, float temperature)
+		{
+			_apiKey = apiKey;
+			_chatbotName = chatBotName;
+			_initialPrompt = initialPrompt;
+			_maxTokens = maxTokens;
+			_temperature = temperature;
+		}
+	}
+}
